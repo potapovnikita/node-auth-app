7f80678a07d6d51cdbb624a7e6b756beb871b46d
potapov.nikita <potapov.nikita@itmh.ru>
2019-01-31 01:19:47 +0500

    revert

--- lib/server.d.ts
+++
@@ -1,7 +0,0 @@
-declare const express: any;
-declare const MongoClient: any;
-declare const bodyParser: any;
-declare const logger: any;
-declare const cors: any;
-declare const app: any;
-declare const port: string | number;
